ba6ba79215ba8b662f0954642995fc0c0e66668d
Luca <luca@MacBookdiLuca2.station>
2018-12-16 12:14:53 +0100

    prima form

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,51 @@
+<?php
+// src/Controller/DefaultController.php
+namespace App\Controller;
+
+use App\Entity\Task;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/form/new")
+    */
+    public function new(Request $request)
+    {
+        // creates a task and gives it some dummy data for this example
+        $task = new Task();
+        $task->setTask('Write a blog post');
+        $task->setDueDate(new \DateTime('tomorrow'));
+
+        $form = $this->createFormBuilder($task)
+            ->add('task', TextType::class)
+            ->add('dueDate', DateType::class)
+            ->add('save', SubmitType::class, array('label' => 'Create Task'))
+            ->getForm();
+            
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $task = $form->getData();
+
+            // ... perform some action, such as saving the task to the database
+            // for example, if Task is a Doctrine entity, save it!
+            // $entityManager = $this->getDoctrine()->getManager();
+            // $entityManager->persist($task);
+            // $entityManager->flush();
+
+            return $this->redirectToRoute('app_lucky_number');
+        }
+
+        return $this->render('default/new.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+}
